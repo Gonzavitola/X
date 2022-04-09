ee924175393ac2569b6edc1d59915f84d498b91d
Gonzavitola <laylapuches@gmail.com>
2022-04-09 10:33:41 -0300

    tp1 complete

---
+++ Assets/tp1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+public class tp1 : MonoBehaviour
+{
+    public int Numero1,Numero2;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Numero1 == Numero2)
+        {
+            Debug.Log("Numero 1 es igual a Numero 2");
+    
+        }
+        if (Numero1 != Numero2)
+        {
+            Debug.Log("Numero 1 no es igual a Numero 2");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
